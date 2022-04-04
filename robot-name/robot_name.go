eb9e5d2f9dfc353bf51af5791a00222c50987f35
Matthias Ruhland <matthias@Matthiass-MacBook-Pro.local>
2022-04-03 22:27:03 -0600

    updated robot-name exercise to make it more efficient

--- robot-name/robot_name.go
+++ robot-name/robot_name.go
@@ -4,80 +4,68 @@ import (
 	"errors"
 	"fmt"
 	"math"
-	"math/rand"
-	"time"
 )
 
-// idea:
-// generate a map of sequential names
-// assign one after another
-
-// Define the Robot type here.
 type Robot struct {
 	name string
 }
 
-var usedNames = make(map[string]bool)
+var names = make(map[int]string)
 
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 const numberOfLetters = 2
 const numberOfNumbers = 3
 
 var maxValidNames = int(math.Pow(float64(len(letters)), numberOfLetters) * math.Pow(10.0, numberOfNumbers))
+var currentNameIndex = 0
+
+func init() {
+	createNames()
+}
 
 func (r *Robot) Name() (string, error) {
+	var err error
 	if r.name == "" {
-		r.Reset()
+		err = r.Reset()
+	}
+	if err != nil {
+		return "", err
 	}
 	return r.name, nil
 }
 
-func (r *Robot) Reset() {
-	r.name = createUniqueName()
+func (r *Robot) Reset() error {
+	var err error
+	r.name, err = getNewName()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func createUniqueName() string {
-	var name string = ""
-	var valid bool = false
-	var err error = nil
-	for !valid && err == nil {
-		name = createRandomLetters(numberOfLetters) + createRandomNumberString(numberOfNumbers)
-		valid, err = validName(name)
+func getNewName() (string, error) {
+	if currentNameIndex == maxValidNames {
+		fmt.Println("out of names")
+		return "", errors.New("out of valid names")
 	}
-	usedNames[name] = true
-	return name
+	if _, ok := names[currentNameIndex]; !ok {
+		return "", errors.New("couldn't retrieve name")
+	}
+	name := names[currentNameIndex]
+	currentNameIndex++
+	return name, nil
 }
 
-func createRandomLetters(n int) string {
-	var l []byte
-	for i := 0; i < n; i++ {
-		j := getRandomNumber(len(letters))
-		l = append(l, letters[j])
+func createNames() {
+	counter := 0
+	for i := 0; i < len(letters); i++ {
+		for j := 0; j < len(letters); j++ {
+			for n := 0; n < 1000; n++ {
+				name := fmt.Sprintf("%s%s%03d", string(letters[i]), string(letters[j]), n)
+				names[counter] = name
+				counter++
+			}
+		}
 	}
-	return string(l)
-}
-
-func createRandomNumberString(n int) string {
-	var ns string
-	for i := 0; i < n; i++ {
-		ns = ns + fmt.Sprintf("%d", getRandomNumber(10))
-	}
-	return ns
-}
-
-func getRandomNumber(n int) int {
-	rand.Seed(time.Now().UnixNano())
-	return rand.Intn(n)
-}
-
-func validName(name string) (bool, error) {
-	fmt.Println("max valid names:", maxValidNames)
-	fmt.Println("names allocated:", len(usedNames))
-	if len(usedNames) == maxValidNames {
-		return false, errors.New("out of valid names")
-	}
-	if _, exists := usedNames[name]; exists {
-		return false, nil
-	}
-	return true, nil
 }
